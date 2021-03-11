c5ba2eb33799425b3bba331a40025491f9fad6b2
m33x0 <michael.mbea@gmail.com>
2021-03-11 20:31:52 +0100

    common ancestor

---
+++ 101-binary_tree_levelorder.c
@@ -0,0 +1,29 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_levelorder - tree level order
+ * @tree: a tree
+ * @func: a func
+ * Return: nothing
+ */
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
+{
+
+	if (tree && func)
+	{
+		if (tree->parent == NULL)
+		{
+			func(tree->n);
+		}
+		if (tree->left != NULL)
+		{
+			func(tree->left->n);
+		}
+		if (tree->right != NULL)
+		{
+			func(tree->right->n);
+		}
+		binary_tree_levelorder(tree->left, func);
+		binary_tree_levelorder(tree->right, func);
+	}
+}
